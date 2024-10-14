33b896e7ca1cdca0288555f49088c1f5d8ce28aa
Sebastian <azerdumat@gmail.com>
2024-10-14 16:30:34 +0200

    search functionality + filters (artists, albums, etc)

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -2,15 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\Song;
-use App\Entity\Track;
-use App\Form\TrackFormType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Service\AuthSpotifyService;
 use Doctrine\Common\Lexer\Token;
+use PHPUnit\TextUI\XmlConfiguration\CodeCoverage\Report\Text;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 
 class SongController extends AbstractController
@@ -25,27 +27,50 @@ class SongController extends AbstractController
     }
 
     #[Route('/song', name: 'app_song')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        // Make the GET request to the Spotify API with kazzey as the query and the token as the Authorization header
-        $response = $this->httpClient->request('GET', 'https://api.spotify.com/v1/albums/1PuCoaLQNyCeaLBgGtNyW3?si=QYmhIfK_SiCK7cwOmFRKAA', [
-            'headers' => [
-                'Authorization' => 'Bearer ' . $this->token,
-            ],
-        ]);
+        $results=null;
+        $search_param=$request->query->all();
+        $defaultData = ['message' => 'Type your message here'];
+        $options=['album'=>'album','artist'=>'artist','playlist'=>'playlist','track'=>'track','show'=>'show','episode'=>'episode','audiobook'=>'audiobook'];
+        $form = $this->createFormBuilder($defaultData)
+            ->add('search', TextType::class)
+            ->add('type',ChoiceType::class,[
+                'choices'  => $options,
+            ])
+            ->add('send', SubmitType::class)
+            ->getForm();
+        $form->handleRequest($request);
 
-        //dd($response->toArray()['tracks']['items']);
-        $tracks=$response->toArray()['tracks']['items'];
-        //$tracks = $this->trackFactory->createMultipleFromSpotifyData($response->toArray()['tracks']['items']);
+        if($form->isSubmitted() && $form->isValid()){
+            return $this->redirectToRoute('app_song', ['search' => $form->getData()['search'],'type'=>$form->getData()['type']]);
+        }
 
+        if($search_param) {
 
-        // creates a Song object and initializes some data for this example
-        $track = new Track('');
-        $form = $this->createForm(TrackFormType::class, $track);
+            $type=$search_param['type'];
+            $endpoint="https://api.spotify.com/v1/search?";
+            $params=[
+                'q'=>$search_param['search'],
+                'type'=>$type,
+                'market'=>'ES',
+                'limit'=>'10',
+                'offset'=>'1',
+                'include_external'=>'audio',
+            ];
+
+            $query=http_build_query($params);
+            $response = $this->httpClient->request('GET', $endpoint.$query, [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->token,
+                ],
+            ]);
+            $results=$response->toArray()[$type.'s']['items'];
+        }
 
         return $this->render('song/index.html.twig', [
             'controller_name' => 'SongController',
-            'tracks' => $tracks,
+            'results' => $results,
             'form' => $form,
         ]);
     }
